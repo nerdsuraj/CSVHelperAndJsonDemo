84e1b738c0db41fe0903f8b17c76ce6cda9df80f
suraj <soorajkrpandit@gmail.com>
2022-05-29 12:15:55 +0530

    Add project files.

---
+++ CSVHelperAndJson/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVHelperAndJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("# Read data from CSV & Write data in CSV");
+            CSVHandler.ImplementCSVDataHandling();
+            //ReadCSV_And_WriteJSON.ImplementCSVToJSON();
+            Console.ReadKey();
+        }
+    }
+}
